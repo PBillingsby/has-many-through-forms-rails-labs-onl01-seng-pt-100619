41a2ca6a07c4438365e66cd36d181d9e52832354
PBillingsby <philbillingsby@gmail.com>
2020-10-03 15:55:25 -0700

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,15 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
+    # comment.user = User.find_or_create_by(username: params[:comment][:user][:username])
     redirect_to comment.post
   end
+
+    # def update
+    #   comment= Comment.find_by(id: params[:id])
+    #   comment.update(comment_params)
+    #   redirect_to post_path(comment.post)
+    # end
 
   private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: 'Phil')
+Category.create(name: 'Drama')
+Post.create(title: 'This is good', content: 'This is content', category_ids: [1])
